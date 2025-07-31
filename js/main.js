f55e94e7452eb7210a2678645fb426b3ef2fb3d3
kass-98 <kassandra.negretec@gmail.com>
2025-07-31 16:42:14 -0600

    Se terminó el objetivo de agregar elementos en una tabla

--- js/main.js
+++ js/main.js
@@ -2,18 +2,21 @@ const btnAgregar = document.getElementById("btnAgregar");
 const btnClear = document.getElementById("btnClear");
 const txtName = document.getElementById("Name");
 const txtNumber = document.getElementById("Number");
+
 const alertValidaciones = document.getElementById("alertValidaciones");
 const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const tablaListaCompras = document.getElementById("tablaListaCompras");
+const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
+
+let cont = 0;
 
 function validarCantidad(){
     if(txtNumber.value.length==0){
         return false;
     }
-
     if(isNaN(txtNumber.value)){
         return false;
     }
-    
     if(Number(txtNumber.value)<=0){
         return false;
     }
@@ -26,6 +29,7 @@ function getPrecio(){
 
 btnAgregar.addEventListener("click", function(event){
     event.preventDefault();
+    let isValid = true;
     alertValidacionesTexto.innerHTML="";
     alertValidaciones.style.display="none";
     txtName.style.border="";
@@ -40,6 +44,24 @@ btnAgregar.addEventListener("click", function(event){
         txtNumber.style.border="thin red solid";
         alertValidacionesTexto.innerHTML += "<strong>La cantidad no es correcta</strong>";
         alertValidaciones.style.display="block";
+        isValid =false;
+    }
+
+    if(isValid){
+        cont++;
+        let precio = getPrecio();
+        let row=`<tr>
+                    <td>${cont}</td>
+                    <td>${txtName.value}</td>
+                    <td>${txtNumber.value}</td>
+                    <td>${precio}</td>
+                </tr>
+        `;
+        cuerpoTabla.insertAdjacentHTML("beforeend", row);
+        txtName.value ="";
+        txtNumber.value="";
+        txtName.focus();
     }
 
 });
+
